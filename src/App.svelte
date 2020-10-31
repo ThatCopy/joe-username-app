6cb129146ec450ff41d0f7cc048f38f8dc821f20
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-31 14:00:17 +0100

    Add all the stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-	export let name: string;
+import { generate } from "joe-username";
+
+let name = generate();
+
+function handleClick() {
+	name = generate();
+}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 id="heading">Joe Username Generator</h1>
+	<h2>{name}</h2>
+	<button on:click={handleClick}>Generate</button>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	#heading{
+		font-size: 300%;
 	}
 </style>
